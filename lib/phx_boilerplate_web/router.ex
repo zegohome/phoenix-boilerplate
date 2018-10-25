38d7a978dd1ace4885217dba601a7369d50a72e0
techgaun <coolsamar207@gmail.com>
2018-10-25 10:07:01 -0500

    add example scope/pipeline

--- lib/phx_boilerplate_web/router.ex
+++ lib/phx_boilerplate_web/router.ex
@@ -1,11 +1,21 @@
 defmodule PhxBoilerplateWeb.Router do
   use PhxBoilerplateWeb, :router
 
+  # add auth plug for basic auth
+  pipeline :api_auth do
+    # plug PhxBoilerplate.Auth
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/api", PhxBoilerplateWeb do
     pipe_through :api
+  end
+
+  scope "/api", PhxBoilerplateWeb do
+    pipe_through :api
+    pipe_through :api_auth
   end
 end
