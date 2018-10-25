e46ac14950d59f83779f09f3029192baf7a08a5a
techgaun <coolsamar207@gmail.com>
2018-10-25 09:57:26 -0500

    fix warning

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule PhxBoilerplate.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
